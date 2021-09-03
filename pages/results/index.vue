4f89cce428cf68d1a20b75790c9b9b718d176115
Jan Kamienicki <nowak480@gmail.com>
2021-09-03 10:47:18 +0200

    Feature: results index page

--- pages/results/index.vue
+++ pages/results/index.vue
@@ -1,30 +1,42 @@
 <template>
-  <article v-if="!isLoading" class="resultsPage">
-    <div
+  <article class="resultsPage">
+    <nuxt-link
       v-for="result in results"
+      :to="`results/${extractMovieId(result.id)}`"
       :key="result.id"
       class="resultsPage_result"
     >
       <div class="result_imgContainer">
-        <img v-if="result.image" :src="result.image.url" alt="movie image" class="imgContainer_img">
-      </div>
-      <p class="result_title">{{ result.title }}</p>
-      <div class="result_details">
-        <p class="details_year">{{ result.year }}</p>
-        <p class="details_type">{{ result.titleType }}</p>
-        <p class="details_episodes">Episodes: {{ result.numberOfEpisodes }}</p>
-      </div>
-      <div class="result_principals">
-        <header class="principals_title">Actors</header>
-        <p
-          v-for="principal in result.principals"
-          :key="principal.name"
-          class="principals_principal"
+        <img
+          v-if="result.image"
+          :src="result.image.url"
+          alt="movie image"
+          class="imgContainer_img"
         >
-          {{ principal.name }}
-        </p>
       </div>
-    </div>
+      <div class="result_content">
+        <p class="content_title">{{ result.title }}</p>
+        <div class="content_details">
+          <p class="details_year">{{ result.year }}</p>
+          <p class="details_type">
+            {{ result.titleType ? capitalize(result.titleType) : null }}
+          </p>
+          <p class="details_episodes">
+            Episodes: {{ result.numberOfEpisodes || 1 }}
+          </p>
+        </div>
+        <div class="content_principals" v-if="result.principals">
+          <header class="principals_title">Actors</header>
+          <p
+            v-for="principal in result.principals"
+            :key="principal.name"
+            class="principals_principal"
+          >
+            {{ principal.name }}
+          </p>
+      </div>
+      </div>
+    </nuxt-link>
   </article>
 </template>
 
@@ -38,6 +50,16 @@ export default {
     }
   },
 
+  methods: {
+    capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+
+    extractMovieId(fullId) {
+      const regex = new RegExp('/title/', 'g');
+      return fullId.replace(regex, '');
+    },
+  },
 
   async fetch() {
     const response = await this.$store.dispatch('movies/getResultsBySearchTerm', this.$route.query.term);
